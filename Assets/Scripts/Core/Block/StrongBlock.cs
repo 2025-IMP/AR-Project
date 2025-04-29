687760fb79872ad8beb125ab35131a96c3fdd32a
Dongjin Kuk <85764911+puter2712git@users.noreply.github.com>
2025-04-30 02:09:22 +0900

    Fix strong block logic

--- Assets/Scripts/Core/Block/StrongBlock.cs
+++ Assets/Scripts/Core/Block/StrongBlock.cs
@@ -7,7 +7,16 @@ namespace IMP.Core
 {
     public class StrongBlock : Block
     {
+        private Renderer m_Renderer;
+        private float m_MaxR = 255f;
+        private float m_MinR = 39f;
+
         private int m_HitCount = 0;
+
+        void Awake()
+        {
+            m_Renderer = GetComponentInChildren<Renderer>();
+        }
 
         public override void Initialize()
         {
@@ -24,6 +33,14 @@ namespace IMP.Core
                 {
                     m_Rigidbody.isKinematic = false;
                     m_Rigidbody.useGravity = true;
+                }
+                else
+                {
+                    float ratio = 1f - (m_HitCount / 2f);
+
+                    Color rcolor = m_Renderer.material.color;
+                    rcolor.r = ratio;
+                    m_Renderer.material.color = rcolor;
                 }
             }
         }
