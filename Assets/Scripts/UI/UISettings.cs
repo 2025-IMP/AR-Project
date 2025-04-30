ae24546f4cb58c7f650866bcf96dc16cdd6c252a
Dongjin Kuk <85764911+puter2712git@users.noreply.github.com>
2025-04-30 16:51:20 +0900

    Apply some designs in intro scene

--- Assets/Scripts/UI/UISettings.cs
+++ Assets/Scripts/UI/UISettings.cs
@@ -11,17 +11,17 @@ namespace IMP.UI
 
         private void OnEnable()
         {
-            m_BGMSlider.value = AudioManager.Instance.VolumeDict[AudioCategory.BGM];
-            m_SFXSlider.value = AudioManager.Instance.VolumeDict[AudioCategory.SFX];
+            m_BGMSlider.value = AudioManager.Instance.VolumeDict[AudioCategory.BGM] * 10;
+            m_SFXSlider.value = AudioManager.Instance.VolumeDict[AudioCategory.SFX] * 10;
         }
 
         public void OnBGMSliderChanged()
         {
-            AudioManager.Instance.SetVolume(AudioCategory.BGM, m_BGMSlider.value);
+            AudioManager.Instance.SetVolume(AudioCategory.BGM, m_BGMSlider.value * 0.1f);
         }
         public void OnSFXSliderChanged()
         {
-            AudioManager.Instance.SetVolume(AudioCategory.SFX, m_SFXSlider.value);
+            AudioManager.Instance.SetVolume(AudioCategory.SFX, m_SFXSlider.value * 0.1f);
         }
 
         public void OnBackButtonPressed()
